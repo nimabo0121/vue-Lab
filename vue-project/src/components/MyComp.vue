8676c0f118fb99ac765ff319ad049fd421722699
nimabo0121 <nimabo0121@gmail.com>
2025-01-07 08:03:52 +0800

    123
    123

---
+++ vue-project/src/components/MyComp.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const props = defineProps({
+    greetingMessage:{
+        type: String,
+        required:true
+    },
+    obj:{
+        type: Object,
+        required:true,
+        validator:(value)=>{
+            return value.count < 5
+        }
+    }
+});
+
+const count = ref(props.obj.count)
+
+</script>
+
+<template>
+    {{ props.greetingMessage }}
+    {{ count }}
+    <button @click="count++">
+        change
+        </button>
+</template>
--- vue-project/src/components/Vue-components .vue
+++ vue-project/src/components/Vue-components .vue
@@ -1,13 +1,56 @@
 <script setup>
+import { ref } from 'vue';
+import MyComp from './MyComp.vue';
+
+const msg = ref('Hello World')
+
+const props = {
+    greetingMessage: "123",
+    obj: {
+        count: 1
+    }
+}
+
 
 </script>
 
 <template>
-    <p>Vue-components </p>
+    <div class="greetings">
+        <h1>組件註冊、屬性與事件</h1>
+        <a href="https://vuejs.org/guide/components/registration.html">vue-components</a>
+        <h2>{{ msg }}</h2>
+        <input v-model="msg" />
+        <!-- <br /> -->
+        <!-- <MyComp greetingMessage="123" />  -->
+        <!-- <br /> -->
+        <!-- 官方建議使用 kebab-case -->
+        <!-- <MyComp greeting-message="456" />  -->
+        <br />
+        <h3>使用 v-bind</h3>
+        <a href="https://vuejs.org/guide/components/props.html">props</a>
+        <MyComp v-bind="props" />
+        <br />
+        <MyComp greeting-message="456" :obj="props.obj" />
+        <br />
+        <img src="/src/assets/7.png" alt="" />
+
+        <h1>prop 校驗</h1>
+        <img src="/src/assets/8.png" alt="">
+        <!-- <br /> -->
+        <img src="/src/assets/9.png" alt="">
+        <img src="/src/assets/10.png" alt="">
+        <br />
+        <p style="text-align: center;">validator 範例</p>
+        <p> 自定義校驗函數, 這邊function寫在子元件中,<span style="color: red;">
+                校驗父元件傳入參數 count < 5 </span>
+        </p>
+        <MyComp greeting-message="456" :obj="{ count: 4 }" />
+
+
+    </div>
 </template>
 
 <style>
-
 button,
 a {
     display: block;
@@ -20,6 +63,7 @@ a {
 }
 
 @media (min-width: 1024px) {
+
     .greetings h1,
     .greetings h3 {
         text-align: center;
